b3271c714550af405207ad8369ca682038215bee
Tatiana <k.tanya@tut.by>
2021-08-24 17:18:30 +0300

    added styles inputRange,  separated styles global.scss

---
+++ src/js/inputRange.js
@@ -0,0 +1,10 @@
+$('input[type="range"]').change(function () {
+    var val = ($(this).val() - $(this).attr('min')) / ($(this).attr('max') - $(this).attr('min'));
+    
+    $(this).css('background-image',
+                '-webkit-gradient(linear, left top, right top, '
+                + 'color-stop(' + val + ', #35CC79), '
+                + 'color-stop(' + val + ', #C4C4C4)'
+                + ')'
+                );
+});
